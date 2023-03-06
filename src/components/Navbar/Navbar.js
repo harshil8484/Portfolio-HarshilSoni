3b497bac7afd25be1b8690ee499de51759cc6ddb
harshil8484 <harshilsoni888@gmail.com>
2023-03-06 15:37:26 +0530

    add new block

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import classes from "./Navbar.module.css";
+import github from "./github.png";
+import linkedin from "./linkedin.png";
+
 const Navbar = () => {
   const [navbarStick, setNavBarStick] = useState(false);
   useEffect(() => {
@@ -27,8 +30,11 @@ const Navbar = () => {
       <div className={classes["navbar-content"]}>
         <div className={classes.links}>
           <ul>
-            <li className={classes.logo}>Harshil Soni</li>
-            <li
+            <li className={classes.logo}>
+              <span>harshil.</span>
+              <span>ui developer</span>  
+            </li>
+            {/* <li
               onClick={() => {
                 scrollToHandler("projects");
               }}
@@ -41,18 +47,24 @@ const Navbar = () => {
               }}
             >
               Skills
-            </li>
+            </li> */}
           </ul>
         </div>
+        <div className="middle-nav">
+            <a href="https://www.linkedin.com/in/harshil-soni-2b02669b/" target="_blank">  
+              <img src={github}/>
+            </a>
+            <a href="https://github.com/harshil8484" target="_blank"> 
+              <img src={linkedin}/>
+            </a>
+        </div>
         <div className={classes["hire-me"]}>
-          <button
-            onClick={() => {
-              scrollToHandler("contactus");
-            }}
-            className="contact-me"
+        <a
+           href="mailto:harshilsoni888@gmail.com" 
+            className="contact-me email"
           >
-            Contact
-          </button>
+            email me
+          </a>
         </div>
       </div>
     </header>
